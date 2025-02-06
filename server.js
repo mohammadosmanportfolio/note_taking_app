c4605a40f9d699f7353ca661879aec7eb7058407
mohammadosman <mohammadhakimosman@outlook.com>
2025-02-05 22:03:46 -0800

    added MongoDB connection

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
-import mongoose from 'mongoose';
+import mongoose, { mongo } from 'mongoose';
 import dotenv from 'dotenv'
 
 // Load environment variables
@@ -13,6 +13,20 @@ app.use(express.json());
 
 // Middleware to log requests
 app.use(morgan('dev'));
+
+// Mongo URI for database connection
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.error("Error connecting to MongoDB: ", error);
+});
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send("Hello World new");
