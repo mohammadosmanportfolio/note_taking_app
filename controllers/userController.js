350ea0c866e3cb2a8f103b0de04a7c44386914a1
mohammadosman <mohammadhakimosman@outlook.com>
2025-02-08 10:56:47 -0800

    more changes to controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,11 +10,23 @@ dotenv.config();
 const registerUser = async (req, res) => {
     const username = req.body["username"];
     const password = req.body["password"];
+    console.log(`Registering user ${username}`);
     
-    const newUser = await User.findOne(username);
-    if (!newUser){
-        res.status(400).json({message: "User already exists"});
+    const existingUser = await User.findOne(username);
+    if (existingUser){
+       return res.status(400).json({message: "User already exists"});
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+        username: username,
+        password: hashedPassword
+    });
+
+    await newUser.save();
+    console.log(`New user saved: ${newUser}`);
 
     console.log(`Username: ${username}`);
     console.log(`Password: ${password}`);
@@ -25,7 +37,36 @@ const loginUser = async(req, res) => {
     const username = req.body["username"];
     const password = req.body["password"];
     console.log("in login user controller");
-}
+
+    try{
+    // Checking if the user doesn't exist
+    const existingUser = await User.findOne(username);
+    if(!existingUser){
+        console.log(`Invalid credentials ${username}`);
+        return res.status(400).json({message: "Invalid credentials"});
+    }
+
+    // Checking if the password the user entered is correct
+    const isMatch = await bcrypt.compare(password, existingUser.password);
+    if (!isMatch){
+        console.log(`Invalid credentials ${username}`);
+        return res.status(400).json({message: "Invalid credentials"});
+    }
+
+    const token = jwt.sign(
+        {userId: existingUser._id},
+        process.env.JWT_KEY,
+        {expiresIn: '1h'}
+    );
+
+    // User has successfully logged in at this point
+    console.log(`User has successfully logged in ${username}`);
+    res.status(200).json({token})
+    }catch(error){
+        console.log(`Error logging in user: ${error}`);
+        res.status(500).json({message: "Server error"});
+    }
+};
 
 // Logic for logging in a user
 //const loginUser = ();
